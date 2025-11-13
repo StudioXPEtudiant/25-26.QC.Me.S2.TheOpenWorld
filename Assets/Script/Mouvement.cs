d789a8f5ac76b864ef388a4662bb84f90a3a880a
Yuhang Zhang <Yuhangz208@gmail.com>
2025-11-12 19:55:01 -0500

    Spam Jumping Issue

--- Assets/Script/Mouvement.cs
+++ Assets/Script/Mouvement.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
+    [Header("Movement Settings")]
     [SerializeField] private float speed = 5f;
     [SerializeField] private float rotationSpeed = 10f;
+
+    [Header("Jump Settings")]
     [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float jumpCooldown = 0.5f; // wait between jumps
+
+    [Header("Crouch Settings")]
     [SerializeField] private float crouchHeight = 0.5f;
     [SerializeField] private float crouchSpeed = 5f;
+    [SerializeField] private float crouchCooldown = 0.3f; // prevent spam crouching
 
     private Rigidbody rb;
     private Vector3 originalScale;
     private Vector3 crouchedScale;
     private bool isCrouching;
+    private bool canJump = true;
+    private bool canCrouch = true;
 
     private void Start()
     {
@@ -28,8 +37,8 @@ public class Mouvement : MonoBehaviour
 
     private void Update()
     {
-        Jump();
-        Crouch();
+        HandleJump();
+        HandleCrouch();
     }
 
     private void Move()
@@ -50,33 +59,52 @@ public class Mouvement : MonoBehaviour
         }
     }
 
-    private void Jump()
+    private void HandleJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            canJump = false;
+            Invoke(nameof(ResetJump), jumpCooldown);
         }
     }
 
-    private void Crouch()
+    private void HandleCrouch()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) && !isCrouching && canCrouch)
         {
-            if (!isCrouching)
-            {
-                transform.localScale = Vector3.Lerp(transform.localScale, crouchedScale, Time.deltaTime * crouchSpeed);
-                isCrouching = true;
-            }
+            StopAllCoroutines();
+            StartCoroutine(SmoothCrouch(true));
         }
-        else
+        else if (!Input.GetKey(KeyCode.LeftShift) && isCrouching && canCrouch)
         {
-            if (isCrouching)
-            {
-                transform.localScale = Vector3.Lerp(transform.localScale, originalScale, Time.deltaTime * crouchSpeed);
-                if (Vector3.Distance(transform.localScale, originalScale) < 0.01f)
-                    transform.localScale = originalScale;
-                isCrouching = false;
-            }
+            StopAllCoroutines();
+            StartCoroutine(SmoothCrouch(false));
         }
+    }
+
+    private System.Collections.IEnumerator SmoothCrouch(bool crouch)
+    {
+        canCrouch = false;
+        Vector3 targetScale = crouch ? crouchedScale : originalScale;
+        Vector3 startScale = transform.localScale;
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * crouchSpeed;
+            transform.localScale = Vector3.Lerp(startScale, targetScale, t);
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
+        isCrouching = crouch;
+        yield return new WaitForSeconds(crouchCooldown);
+        canCrouch = true;
+    }
+
+    private void ResetJump()
+    {
+        canJump = true;
     }
 }
