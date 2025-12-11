abf540dffdc71225ffbda25487acec5159d14055
Yuhang Zhang <Yuhangz208@gmail.com>
2025-12-10 19:48:58 -0500

    fix update

--- Assets/Script/Sword.cs
+++ Assets/Script/Sword.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class SwordAttack : MonoBehaviour
 {
+    public Transform body;
     public float attackDistance = 0.5f;
     public float attackSpeed = 10f;
     public KeyCode attackKey = KeyCode.E;
 
-    Vector3 originalPosition;
+    Vector3 originalLocalPos;
     bool isAttacking = false;
 
     void Start()
     {
-        originalPosition = transform.localPosition;
+        originalLocalPos = transform.localPosition;
     }
 
     void Update()
@@ -24,16 +25,18 @@ public class SwordAttack : MonoBehaviour
     {
         isAttacking = true;
 
-        Vector3 targetPos = originalPosition + transform.forward * attackDistance;
+        Vector3 dir = body.forward;
+        Vector3 targetPos = originalLocalPos + dir.normalized * attackDistance;
+
         while (Vector3.Distance(transform.localPosition, targetPos) > 0.01f)
         {
             transform.localPosition = Vector3.Lerp(transform.localPosition, targetPos, Time.deltaTime * attackSpeed);
             yield return null;
         }
 
-        while (Vector3.Distance(transform.localPosition, originalPosition) > 0.01f)
+        while (Vector3.Distance(transform.localPosition, originalLocalPos) > 0.01f)
         {
-            transform.localPosition = Vector3.Lerp(transform.localPosition, originalPosition, Time.deltaTime * attackSpeed);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, originalLocalPos, Time.deltaTime * attackSpeed);
             yield return null;
         }
 
